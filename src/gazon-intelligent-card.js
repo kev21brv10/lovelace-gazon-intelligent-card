4e534d03bbcf7fb001cd75e219f4fc6f98c7fba9
kev21brv10 <110838242+kev21brv10@users.noreply.github.com>
2026-04-03 14:08:52 +0200

    chore(card): bump version to 0.1.46

--- src/gazon-intelligent-card.js
+++ src/gazon-intelligent-card.js
@@ -10,7 +10,7 @@ import {
 
 const CARD_TYPE = "gazon-intelligent-card";
 const CARD_NAME = "Gazon Intelligent Card";
-const CARD_VERSION = "0.1.45";
+const CARD_VERSION = "0.1.46";
 
 const DEFAULT_CONFIG = {
   title: "Gazon Intelligent",

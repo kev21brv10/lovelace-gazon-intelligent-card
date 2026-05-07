6608f2546e9862b639a41b90fe2257c851cf4715
kev21brv10 <110838242+kev21brv10@users.noreply.github.com>
2026-05-07 03:37:22 +0200

    chore: release 0.1.100

--- gazon-intelligent-card.js
+++ gazon-intelligent-card.js
@@ -3982,7 +3982,7 @@ const EDITOR_STYLES = String.raw`
 
 const CARD_TYPE = "gazon-intelligent-card";
 const CARD_NAME = "Gazon Intelligent Card";
-const CARD_VERSION = "0.1.99";
+const CARD_VERSION = "0.1.100";
 
 const DEFAULT_CONFIG = {
   title: "Gazon Intelligent",
